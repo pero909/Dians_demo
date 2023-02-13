7de92610f9de0484deea5837f2026a3bb01e326d
Petar Vasev <35383291+pero909@users.noreply.github.com>
2023-02-13 17:26:56 +0100

    Update LocationController.java

--- src/main/java/com/example/dians_demo/Controllers/LocationController.java
+++ src/main/java/com/example/dians_demo/Controllers/LocationController.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping({"/","","/Navster"})
+@RequestMapping({"/"})
 public class LocationController {
 
     private final LocationService locationService;
